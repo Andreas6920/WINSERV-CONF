c126319c8d783ef97a3ad000e1dc624f5cb21abf
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2021-09-19 05:37:30 +0200

    first run

---
+++ Server-part1.ps1
@@ -0,0 +1,102 @@
+CLS
+## PART 1 - OPSÆTNING
+
+    ## PART 1.1 - COMPUTERNAME
+    CLS
+    "";
+    Write-host "PART 1.1 - PC NAME"
+    Do {
+            Write-Host "`tCurrent PC Name:" -f yellow -nonewline; Write-Host " $env:computername"
+            Write-Host "`tWould you like to rename this PC? (y/n)" -nonewline -f green;
+            $answer = Read-Host " " 
+            Switch ($answer) { 
+                Y {
+                    
+                    Write-Host "`t`tNew PC name" -f yellow -nonewline;
+                    $PCname = Read-Host " "
+                    $WarningPreference = "SilentlyContinue"
+                    Rename-computer -newname $PCname
+                    $WarningPreference = "Continue"
+                    $reboot = $true
+                    Write-Host "`t`tComputer description" -f yellow -NoNewline
+                    $Description = Read-Host " " 
+                    $ThisPCDescription=Get-WmiObject -class Win32_OperatingSystem
+                    $ThisPCDescription.Description=$Description
+                    $ThisPCDescription.put() | out-null
+                    Write-Host "`t`tComputer renamed. PC will reboot after IP configuration." -f yellow
+                    "";
+
+                }
+                N {Write-Host "`t`tNO - This PC will not be renamed." -f red -nonewline; $reboot = $false} 
+            }   
+        } While ($answer -notin "y", "n")    
+    ## PART 1.2 - IP CONFIGURATION
+    "";
+    Write-host "PART 1.2 - IP CONFIGURATION"
+    Do {
+    Write-Host "`tWould you like to change your IP? (y/n)" -nonewline -f green;
+    $answer = Read-Host " " 
+    Switch ($answer) { 
+        Y {
+        ### Choice Network Adapter
+        
+        Do {
+            Write-Host "`t`tCHOOSE NETWORK ADAPTER:" -f yellow; sleep -s 2;
+            "";Write-Host "`t`t`t Your Network Adapters:" -f yellow
+            $nic = Get-NetIPAddress -AddressFamily IPv4
+            foreach ($n in $nic){write-host "`t`t`t" $n.InterfaceAlias "( IP:" $n.IPAddress")"}"";
+            sleep -s 2
+            Write-Host "`t`t`t Please enter the NAME of the primary network card" -nonewline -f yellow;
+            $ethernetadaptername = Read-Host " " 
+            } While ($ethernetadaptername -notin ((Get-NetIPAddress -AddressFamily IPv4).InterfaceAlias)) 
+
+
+
+        ### Get adapter settings
+        "";
+        Write-Host "`t`tSET IP CONFIGURATION:" -f yellow; 
+        $currentip = netsh interface ip show addresses $ethernetadaptername | select-string "IP Address"
+        $currentsubnet = "/"+(Get-NetIPAddress -InterfaceAlias $ethernetadaptername -AddressFamily IPv4).PrefixLength
+        $currentgateway = netsh interface ip show addresses $ethernetadaptername | select-string "Default gateway"
+        $currentDNS = (Get-DnsClientServerAddress -InterfaceAlias Ethernet0).ServerAddresses
+        "";
+        Write-Host "`t`t`t Current IP Settings:" -f yellow
+        Write-Host "`t`t`t Adapter:`t`t`t`t`t`t`t  "$ethernetadaptername
+        Write-Host "`t`t"$currentip
+        Write-Host "`t`t`t Subnet:`t`t`t`t`t`t`t  "$currentsubnet
+        Write-Host "`t`t"$currentgateway[0];"";
+        
+
+        ### Enter New adapter settings
+        Do {Write-Host "`t`t`t Enter new IP Address" -f yellow -NoNewline
+        $newIP = Read-Host " " } While ($newIP -notmatch "\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}")
+        Do {Write-Host "`t`t`t Enter new SUBNET VALUE (example: 24, 16, 8)" -f yellow -NoNewline
+        $newSubnet = Read-Host " "} While ($newSubnet -notin 8..30)
+        Do {Write-Host "`t`t`t Enter new gateway IP:" -f yellow -NoNewline
+        $newGW = Read-Host " "} While ($newGW -notmatch "\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}")
+        Do {Write-Host "`t`t`t Enter new DNS IP:" -f yellow -NoNewline
+        $newDNS = Read-Host " "} While ($newDNS -notmatch "\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}")
+
+        ### Configure new settings
+        "";
+        Write-Host "`t`tNEW CONFIGURATION IS BEING SET:" -f yellow;
+        Write-Host "`t`t`t - Clearing current settings.." -f yellow;
+        Set-ItemProperty -Path “HKLM:\SYSTEM\CurrentControlSet\services\Tcpip\Parameters\Interfaces\$((Get-NetAdapter -InterfaceAlias $ethernetadaptername).InterfaceGuid)” -Name EnableDHCP -Value 0 -ea SilentlyContinue
+        Remove-NetIpAddress -InterfaceAlias $ethernetadaptername -AddressFamily IPv4 -Confirm:$false -ErrorAction SilentlyContinue
+        Remove-NetRoute -InterfaceAlias $ethernetadaptername -AddressFamily IPv4 -Confirm:$false -ErrorAction SilentlyContinue
+        sleep -s 2
+
+        Write-Host "`t`t`t - Setting new IP..." -f yellow; Sleep -s 1
+        Write-Host "`t`t`t - Setting new Subnet..." -f yellow; Sleep -s 1
+        Write-Host "`t`t`t - Setting new Gateway..." -f yellow; Sleep -s 1
+        New-NetIpAddress -InterfaceAlias $ethernetadaptername -IpAddress $newIP -PrefixLength $newSubnet -DefaultGateway $newGW -AddressFamily IPv4 | out-null
+        Write-Host "`t`t`t - Setting new DNS..." -f yellow; Sleep -s 1
+        Set-DnsClientServerAddress -InterfaceAlias $ethernetadaptername -ServerAddresses $newDNS | out-null
+        Write-Host "`t`tIP settings complete!" -f yellow; Sleep -s 1
+        }
+        N{Write-Host "`t`tNo, this step will be skipped." -f red; sleep -s 2;}
+    } }While ($answer -notin "y", "n")
+    
+    if ($reboot -eq $true){
+    Write-Host "`t`tComputer is renamed, rebooting in 5 seconds.." -f yellow; sleep -s 5;
+    Restart-Computer -Force }
---
+++ Server-part2.ps1
@@ -0,0 +1,239 @@
+CLS
+## PART 2 - AD ROLLE OPSÆTNING
+Write-host "PART 2.1 - AD Install"
+
+    ###################################################################
+    ########  Rolle installation
+    #####
+
+    Write-host "`tPART 2.1 - AD Install"
+    Do {
+            Write-Host "`t`tWould you like to deploy a active directory? (y/n)" -nonewline -f green;
+            $answer = Read-Host " " 
+            Switch ($answer) { 
+                Y {
+                    #install AD
+                    $WarningPreference = "SilentlyContinue"
+                    Install-WindowsFeature AD-Domain-Services -IncludeManagementTools | out-null
+                    Write-Host "`t`t`t`tPlease choose a domain name:" -nonewline -f yellow;
+                    $domainname = Read-Host " " 
+                    Import-Module ADDSDeployment
+                    Install-ADDSForest `
+                    -CreateDnsDelegation:$false `
+                    -DatabasePath "C:\Windows\NTDS" `
+                    -DomainMode "WinThreshold" `
+                    -DomainName $domainname `
+                    -DomainNetbiosName ($domainname.split(".")[0]).ToUpper() `
+                    -ForestMode "WinThreshold" `
+                    -Confirm:$false `
+                    -SafeModeAdministratorPassword (ConvertTo-SecureString -AsPlainText "p@ssw0rd" -Force) `
+                    -InstallDns:$true `
+                    -LogPath "C:\Windows\NTDS" `
+                    -NoRebootOnCompletion:$false `
+                    -SysvolPath "C:\Windows\SYSVOL" `
+                    -Force:$true
+                    $WarningPreference = "Continue"
+
+
+                }              
+                N {Write-Host "`t`t`tNO - This step will be skipped." -f red; $reboot = $false} 
+    
+            }   
+    } While ($answer -notin "y", "n")   
+
+## PART 2.2 - AD KONFIGURATION
+Write-host "`tPART 2.2 - AD Konfiguration"
+
+    ###################################################################
+    ########  CSV håndtering
+    #####
+
+    ### STEP 2.2.1 - SOURCE CSV FILE
+    $csvfilepath = "C:\domainusers\domain_users.csv"
+    $Users = Import-Csv -Delimiter ";" -Path $csvfilepath
+    write-host "`t`tCSV fil er placeret i" $csvfilepath -f green
+    write-host "`t`tRediger denne efter behov og tast enter når den er færdig" -f green
+    read-host “`t`tPress ENTER to continue...”
+
+    ###################################################################
+    ########  OU OPRETTELSE
+    #####
+    Write-Host "`t`t`tOU OPRETTELSE:" -f Green
+    foreach($user in $users)
+    {
+    ##Lav værdierne i .csv filen til variabler
+    $rootou=(Get-ADDomain).DistinguishedName 
+    $oupath = $user.ADOU+','+$rootou
+
+    #### Separere landforkortelse fra angivet ousti i .csv fil:
+        $ou_branch1 = $oupath.Split("{,}")[-3] #Dette er med OU tegn
+        $ou_branch2 = $ou_branch1.Split("=")[-1] #Dette er gruppen sepereret
+
+    #### Separere undermappe fra angivet ousti i .csv fil:
+        $ou_user1 = $oupath.Split("{,}")[0] #Dette er med OU tegn
+        $ou_user2 = $ou_user1.Split("=")[-1] #dette er gruppen sepereret
+
+    #### Kombinere stierne
+        $newOU = "$ou_branch1,$rootou" #Branch+rootou
+        $newOU1 = "$ou_user1,$ou_branch1,$rootou" #Users+Branch+rootou
+
+    if (Get-ADOrganizationalUnit -Filter "distinguishedName -eq '$newOU'") ## Findes branch??
+            {if (!(Get-ADOrganizationalUnit -Filter "distinguishedName -eq '$newOU1'"))
+                {Write-Host "`t`t`tOU: " +$ou_user2+"oprettes.." -f yellow
+                New-ADOrganizationalUnit -Name $ou_user2 -Path $newOU -ProtectedFromAccidentalDeletion $False   
+                Write-Host "`t`t- ... '$ou_user2' er nu oprettet!" -f Green
+                }} 
+    else
+            {Write-Host "`t`t`tOU:"$newOU" oprettes.." -f yellow
+            New-ADOrganizationalUnit -Name $ou_branch2 -Path $rootou -ProtectedFromAccidentalDeletion $False
+            New-ADOrganizationalUnit -Name $ou_user2 -Path $newOU -ProtectedFromAccidentalDeletion $False}
+    }
+    write-host "`t`t`tOU: " -f yellow -NoNewline ;
+    write-host "[COMPLETE]" -f Green
+    "";
+    
+    ###################################################################
+    ########  BRUGER OPRETTELSE
+    #####
+
+    Write-Host "`t`t`tBRUGER OPRETTELSE:" -f Green
+    foreach($user in $users)
+    {
+    ##Lav værdierne i .csv filen til variabler##
+    $oupath = $user.ADOU+','+(Get-ADDomain).DistinguishedName
+    $firstName = $user.firstname
+    $lastName = $user.Lastname
+    $displayName = $user.Firstname+' '+$user.Lastname
+    $company = $user.Company
+    $office = $user.Branch
+    $department = $user.Department 
+    $jobtitle = $user.Title
+    $email = $user.'e-mail account'
+    $username = $user.Initials
+    
+    ##Tjek om brugere eksistere, før de oprettes##
+    $Userexists = Get-ADUser -Filter {sAMAccountName -eq $Username}
+    
+    If ($Userexists -eq $Null)
+    {New-ADUser `
+    -Path $oupath `
+    -Department $department `
+    -Title $jobtitle `
+    -Name $username.ToUpper() `
+    -UserPrincipalName $email `
+    -DisplayName "$displayName" `
+    -GivenName "$firstname" `
+    -Surname "$lastname" `
+    -Office $office `
+    -AccountPassword (ConvertTo-SecureString "Pa55w.rd" -AsPlainText -Force) -PassThru| Enable-ADAccount 
+    Write-host `t`t`t"BRUGER: "$firstname $lastname "oprettes.." -f yellow
+    }
+    Else
+    {write-host "`t`t`tBRUGER: $username er allerede optaget af $firstname $lastname." -f red}
+
+    }
+    write-host "`t`t`tBRUGER: " -f yellow -NoNewline ;
+    write-host "[COMPLETE]" -f Green
+    "";
+
+    ###################################################################
+    ########  SECURITY GROUP OPRETTELSE
+    #####
+    Write-Host "`t`t`tSECURITY GROUP OPRETTELSE:" -f Green
+
+    $secfoldername = "SecurityGroups"
+    $sec_root =(Get-ADDomain).DistinguishedName
+    $sec_dest = "OU=$secfoldername,$sec_root"
+
+    # create security group OU and adding department to it
+	write-host "`t`t`tSECURITY GROUP: Creating OU for security groups..." -f Yellow
+    if (!(get-ADOrganizationalUnit -Filter {Name -eq $secfoldername})){
+        New-ADOrganizationalUnit -Name $secfoldername -Path $sec_root -ProtectedFromAccidentalDeletion $False}
+    
+
+    $afdelinger = (get-aduser -filter * -property department).department | Sort-Object -Unique
+    foreach ($afdeling in $afdelinger){
+    $SecGroupName = "SecGroup_"+$afdeling.Replace(" ","")
+    if (!(Get-ADGroup -Filter {Name -eq $SecGroupName})){
+        Write-host "`t`t`tSECURITY GROUP: $SecGroupName oprettet.." -f Yellow
+    New-ADGroup -Name $SecGroupName -SamAccountName $afdeling -GroupCategory Security -GroupScope Global -DisplayName $afdeling -Path "$sec_dest"
+    Write-host "`t`t`tSECURITY GROUP: Tilføjer alle fra $afdeling til $SecGroupName.." -f Yellow
+    Get-ADUser -Filter 'Department -eq $afdeling' | ForEach-Object {Add-ADGroupMember -Identity $afdeling -Members $_ }
+        }}
+    
+    if (!(Get-ADGroup -Filter {Name -eq "SecGroup_CEO"})){
+    New-ADGroup -Name "SecGroup_CEO" -SamAccountName "CEO Management" -GroupCategory Security -GroupScope Global -DisplayName "CEO Management" -Path $sec_dest
+    Get-ADUser -Filter 'Title -eq "CEO"' | ForEach-Object {Add-ADGroupMember -Identity "CEO Management" -Members $_ }}
+    write-host "`t`t`tSECURITY GROUP: " -f yellow -NoNewline ;
+    write-host "[COMPLETE]" -f Green
+    "";
+
+    ###################################################################
+    ########  SMB OPRETTELSE
+    #####
+    Write-Host "`t`t`tSMB OPRETTELSE:" -f Green
+    Do {
+        Write-Host "`t`t`tWould you like to create shares for the departments? (y/n)" -nonewline -f green;
+        $answer = Read-Host " " 
+        Switch ($answer) { 
+            Y {
+    #Lav en mappe til ønskede shares
+    
+    $Path = "C:\"
+    $Name = "user_shares"
+    $SharePath = "$Path$Name\"
+    
+    if(!(test-path $SharePath)) {
+        New-Item -ItemType Directory -Force -Path $SharePath | out-null
+        Write-Host "`t`t`tSMB SHARE: $SharePath er nu oprettet." -f yellow}
+        
+    
+    
+    #Lav shares i den oprettede mappe, Sæt NTFS + Share rettigheder
+    
+    New-Item -ItemType Directory -Force -Path $SharePath\General | out-null
+    $Users = Import-Csv -Delimiter ";" -Path "C:\domainusers\domain_users.csv"
+    foreach($user in $users)
+    {
+    #variabler efter 'department' kolonnen i csv filen.
+            $dep_folders = $user.Department
+            $share_department_specific = "$SharePath$dep_folders"
+    
+    #Lav en mappe pr. afdeling
+            if(!(test-path $share_department_specific)) {
+            Write-host "`t`t`tSMB SHARE: creating $share_department_specific folder" -f yellow
+            New-Item -ItemType Directory -Force -Path $share_department_specific | out-null}
+            
+          
+          #RETTIGHEDER: Shares     
+            New-Smbshare -Name $dep_folders  -Description "Shared folders for $dep_folders and managers" `
+            -Path $share_department_specific -ea SilentlyContinue | out-null
+            Grant-SmbShareAccess -AccountName $dep_folders -Name $dep_folders -AccessRight Change -Force | out-null
+            Grant-SmbShareAccess -AccountName "Management" -Name $dep_folders -AccessRight Read -Force | out-null
+            Grant-SmbShareAccess -AccountName "CEO Management" -Name $dep_folders -AccessRight Change -Force | out-null
+            
+            
+            #RETTIGHEDER: NTFS
+            
+            $ntfs = Get-Acl \\$env:COMPUTERNAME\$dep_folders
+            $AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule("$dep_folders","Modify","Allow")
+            $AccessRule1 = New-Object System.Security.AccessControl.FileSystemAccessRule("Management","Read","Allow")
+            $AccessRule2 = New-Object System.Security.AccessControl.FileSystemAccessRule("CEO Management","Modify","Allow")
+            $ntfs.SetAccessRule($AccessRule) 
+            $ntfs.SetAccessRule($AccessRule1)
+            $ntfs.SetAccessRule($AccessRule2)
+            $ntfs | Set-Acl $share_department_specific 
+            
+            
+        }
+        
+    Write-host "`t`t`tSMB SHARE: Modifying SMB access.." -f yellow
+    Write-host "`t`t`tSMB SHARE: Modifying NTFS permissions.." -f yellow
+    write-host "`t`t`tSMB SHARE: " -f yellow -NoNewline ;
+    write-host "[COMPLETE]" -f Green
+    
+    }              
+            N {Write-Host "`t`t`tNO - This step will be skipped." -f red; $reboot = $false} 
+    
+        }   
+    } While ($answer -notin "y", "n")  
