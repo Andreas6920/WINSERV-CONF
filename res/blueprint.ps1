5295e8155e6a5bcf6d015b064473122582dae380
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2022-05-14 00:32:14 +0200

    test
    tes

---
+++ res/blueprint.ps1
@@ -0,0 +1,21 @@
+AD-user
+
+Choose option:
+
+1 ) Proof of concept - Automatic (From CSV file)
+2 ) Proof of contect - Manual Mode
+3 ) IRL - Automatic mode (From CSV file)
+4 ) IRL - Manual mode
+
+
+
+function 1 {
+
+# Downloading random CSV file
+New-Item
+
+
+
+
+
+}
